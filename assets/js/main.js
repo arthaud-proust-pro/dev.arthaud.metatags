d2b20cb3090ffe0f21778d2f64e8a729c663d382
Arthaud <arthaudp33@gmail.com>
2023-05-08 21:26:52 +0200

    feat: add viewport meta

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,8 @@ function getGeneratedHtml(f) {
 
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        
         <meta name="robots" content="${f.more.robots}">
         <meta name="target" content="${f.more.robots}">
 
